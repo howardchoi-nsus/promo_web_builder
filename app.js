2dffe0d0b9c3f56e06225b9eea019b38553213da
howardchoi-nsus <howard.choi@nsuslab.com>
2026-07-03 17:47:05 +0900

    B섹션 수정

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 (() => {
   const script = document.createElement("script");
-  script.src = "./prototype/app.js";
+  script.src = "./prototype/app.js?v=b-section-template-v2";
   script.defer = true;
   document.head.appendChild(script);
 })();
